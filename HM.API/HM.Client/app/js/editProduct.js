e506626225f2383de80a7b88c51f5d3c240260bb
ljacobs <leo@the-earthworks.com>
2017-02-12 22:16:55 +0000

    Completed CRUD by adding PUT and DELETE

--- HM.API/HM.Client/app/js/editProduct.js
+++ HM.API/HM.Client/app/js/editProduct.js
@@ -5,7 +5,9 @@
             settings: {
                 $modalWrapper: $('.modal-wrapper'),
                 $editBtn: $('.edit'),
-                $deleteBtn: $('.delete')
+                $deleteBtn: $('.delete'),
+                $editForm: $('#editProductForm'),
+                $updateBtn: $('#editProductForm').find('input[type="submit"]')
             },
 
             init: function() {
@@ -16,16 +18,26 @@
             bindUIActions: function() {
                 s.$editBtn.on('click', function(e) {
                     e.preventDefault();
-                    editProduct.editMe($(this));
+                    editProduct.editForm($(this));
                 });
 
                 s.$modalWrapper.on('click', function(e) {
                     e.preventDefault();
-                    $(this).removeClass('reveal');
+                    //$(this).removeClass('reveal');
+                });
+
+                $('.update-button').on('click', function(e) {
+                    e.preventDefault();
+                    editProduct.editMe($(this));
+                });
+
+                $('.delete').on('click', function(e) {
+                    e.preventDefault();
+                    editProduct.deleteMe($(this));
                 });
             },
 
-            editMe: function($clicked) {
+            editForm: function($clicked) {
                 //To show the modal window
                 s.$modalWrapper.addClass('reveal');
                 var id = $clicked.data('id');
@@ -45,20 +57,61 @@
                         $('#productImage').val(image);
                         $('#productPrice').val(price);
                         $('#productStock').val(stock);
+                        $('input[type="button"]').attr('data-id', id);
+
                     },
                     error: function(data, textStatus, jqXHR) {
-                        
+                        console.info(textStatus.statusText);
                     }
-
-
-
                 });
+            },
 
+            editMe: function($clicked) {
+                var id = $clicked.data('id');
+                var editUrl = 'http://api.hm.local/api/Products/' + id;
+
+
+                // Data to be edited
+                var editedProductData = {
+                    "productDescription": $('#productDescription').val(),
+                    "productImage": $('#productImage').val(),
+                    "productPrice": $('#productPrice').val(),
+                    "productStock": $('#productStock').val()
+                };
+                $.ajax({
+                    url: editUrl,
+                    type: 'PUT',
+                    data: editedProductData,
+                    success: function(data, textStatus, jqXHR) {
+                        $('#editProductForm').trigger('reset');
+                        setTimeout(function() {
+                            s.$modalWrapper.removeClass('reveal');
+                        }, 1000);
+                        location.reload();
+                    },
+                    error: function(data, textStatus, jqXHR) {
+                        console.info(textStatus);
+                    }
+                });
 
             },
 
             deleteMe: function($clicked) {
-                
+                var id = $clicked.data('id');
+                var deleteUrl = 'http://api.hm.local/api/Products/' + id;
+
+                $.ajax({
+                    url: deleteUrl,
+                    type: 'DELETE',
+                    success: function(data, textStatus, jqXHR) {
+                        console.info('data successfully deleted');
+                        location.reload();
+                    },
+                    error: function() {
+
+                    }
+                });
+
             }
         };
 
